1419e58c0f796a9450ac97d5161534fdf206685f
Colton Robbins <coltondr@gmail.com>
2023-06-16 21:58:08 -0500

    updated austin monthly species

--- src/api/birds.js
+++ src/api/birds.js
@@ -1,4 +1,114 @@
 const birds = {
+    northernCardinal: {
+        title: "Northern Cardinal",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Prefers millet, but may eat sunflower",
+            2: "May visit bird baths and water sources",
+            3: "Difficult to get at most urban & suburban homes"
+        },
+        yearround: true
+    },
+    carolinaWren: {
+        title: "Carolina Wren",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Prefers millet, but may eat sunflower",
+            2: "May visit bird baths and water sources",
+            3: "Difficult to get at most urban & suburban homes"
+        },
+        yearround: true
+    },
+    bewicksWren: {
+        title: "Bewick's Wren",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Prefers millet, but may eat sunflower",
+            2: "May visit bird baths and water sources",
+            3: "Difficult to get at most urban & suburban homes"
+        },
+        yearround: true
+    },
+    tuftedTitmouse: {
+        title: "Tufted Titmouse",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Prefers millet, but may eat sunflower",
+            2: "May visit bird baths and water sources",
+            3: "Difficult to get at most urban & suburban homes"
+        },
+        yearround: true
+    },
+    blackCrestedTitmouse: {
+        title: "Black-crested Titmouse",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Prefers millet, but may eat sunflower",
+            2: "May visit bird baths and water sources",
+            3: "Difficult to get at most urban & suburban homes"
+        },
+        yearround: true
+    },
+    carolinaChickadee: {
+        title: "Carolina Chickadee",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Prefers millet, but may eat sunflower",
+            2: "May visit bird baths and water sources",
+            3: "Difficult to get at most urban & suburban homes"
+        },
+        yearround: true
+    },
+    northernMockingbird: {
+        title: "Northern Mockingbird",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Prefers millet, but may eat sunflower",
+            2: "May visit bird baths and water sources",
+            3: "Difficult to get at most urban & suburban homes"
+        },
+        yearround: true
+    },
+    blueJay: {
+        title: "Blue Jay",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Loves peanuts",
+            2: "May visit bird baths and water sources",
+            3: "Usually travels in family groups of 2 - 7"
+        },
+        yearround: true
+    },
+    europeanStarling: {
+        title: "European Starling",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Invasive, typically a nuisance at feeders",
+            2: "Drawn to soft foods like suet or de-shelled seeds",
+            3: "Typically travel in small groups"
+        },
+        yearround: true
+    },
+    houseSparrow: {
+        title: "House Sparrow",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Prefers millet, but may eat sunflower",
+            2: "May visit bird baths and water sources",
+            3: "Difficult to get at most urban & suburban homes"
+        },
+        yearround: true
+    },
     paintedBunting: {
         title: "Painted Bunting",
         desc: "Uncommon",
@@ -7,7 +117,8 @@ const birds = {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     rubyThroatedHummingbird: {
         title: "Ruby-throated Hummingbird",
@@ -17,7 +128,8 @@ const birds = {
             1: "Easy to attract with hummingbird feeders",
             2: "Will feed on red, orange, and yellow flowers",
             3: ""
-        }
+        },
+        yearround: false
     },
     blackChinnedHummingbird: {
         title: "Black-chinned Hummingbird",
@@ -27,7 +139,8 @@ const birds = {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     juveniles: {
         title: "Juvenile Birds",
@@ -37,217 +150,283 @@ const birds = {
             1: "Baby birds have hatched, fledged, and left the nest",
             2: "Young follow parents around, learning how to eat and be a bird",
             3: "Commonly seen young include Blue Jay, Titmice, and Cardinals"
-        }
+        },
+        yearround: false
     },
     baltimoreOriole: {
         title: "Baltimore Oriole",
         desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     orchardOriole: {
         title: "Orchard Oriole",
         desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     roseBreastedGrosbeak: {
         title: "Rose-breasted Grosbeak",
         desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
+    },
+    houseFinch: {
+        title: "House Finch",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Eats sunflower, safflower, peanuts",
+            2: "May visit bird baths and water sources",
+            3: "Males show red color, females do not"
+        },
+        yearround: true
+    },
+    lesserGoldfinch: {
+        title: "Lesser Goldfinch",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Loves de-shelled crushed sunflower",
+            2: "May visit bird baths and water sources",
+            3: "Present year-round, but only favors feeders seasonally"
+        },
+        yearround: true
+    },
+    americanGoldfinch: {
+        title: "American Goldfinch",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Loves de-shelled crushed sunflower",
+            2: "May visit bird baths and water sources",
+            3: "Only in Texas during winter months"
+        },
+        yearround: false
     },
     darkEyedJunco: {
         title: "Dark-eyed Junco",
         desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     whiteCrownedSparrow: {
         title: "White-crowned Sparrow",
         desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     clayColoredSparrow: {
         title: "Clay-colored Sparrow",
         desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     grayCatbird: {
         title: "Gray Catbird",
         desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     orangeCrownedWarbler: {
         title: "Orange-crowned Warbler",
-        desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        desc: "Common",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
+    },
+    yellowRumpedWarbler: {
+        title: "Yellow-rumped Warbler",
+        desc: "Common",
+        imgPath: "",
+        details: {
+            1: "Will eat suet and drink from bird baths",
+            2: "",
+            3: ""
+        },
+        yearround: false
     },
     rubyCrownedKinglet: {
         title: "Ruby-crowned Kinglet",
-        desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        desc: "Common",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     goldenFrontedWoodpecker: {
         title: "Golden-fronted Woodpecker",
-        desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        desc: "Common",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: true
     },
     redBelliedWoodpecker: {
         title: "Red-bellied Woodpecker",
-        desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        desc: "Common",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: true
     },
     downyWoodpecker: {
         title: "Downy Woodpecker",
-        desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        desc: "Common",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: true
     },
     ladderBackedWoodpecker: {
         title: "Ladder-backed Woodpecker",
-        desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        desc: "Common",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: true
     },
     yellowBelliedSapsucker: {
         title: "Yellow-bellied Sapsucker",
         desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     northernFlicker: {
         title: "Northern Flicker",
-        desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        desc: "Common",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     cedarWaxwing: {
         title: "Cedar Waxwing",
-        desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        desc: "Common",
+        imgPath: "",
         details: {
-            1: "Prefers millet, but may eat sunflower",
+            1: "Loves to eat berries off trees & shrubs",
             2: "May visit bird baths and water sources",
-            3: "Difficult to get at most urban & suburban homes"
-        }
+            3: "Almost always travel in groups"
+        },
+        yearround: false
     },
     barnSwallow: {
         title: "Barn Swallow",
-        desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        desc: "Common",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     americanRobin: {
         title: "American Robin",
-        desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        desc: "Common",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     houseWren: {
         title: "House Wren",
         desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     nashvilleWarbler: {
         title: "Nashville Warbler",
         desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
     yellowWarbler: {
         title: "Yellow Warbler",
         desc: "Uncommon",
-        imgPath: "/Painted_Bunting-cropped.png",
+        imgPath: "",
         details: {
             1: "Prefers millet, but may eat sunflower",
             2: "May visit bird baths and water sources",
             3: "Difficult to get at most urban & suburban homes"
-        }
+        },
+        yearround: false
     },
 }
 
--- src/api/birdsController.js
+++ src/api/birdsController.js
@@ -7,24 +7,54 @@ birdsController.birds = async (req, res, next) => {
     // define which species are expected during each month, based on region
     const { city } = req.body;
 
-    const janBirds = [];
+    const janBirds = city === "Austin" ? birdsByCity.austin.jan :
+        city === "Dallas" ? birdsByCity.dallas.jan :
+            city === "Houston" ? birdsByCity.houston.jan :
+                city === "San Antonio" ? birdsByCity.sanAntonio.jan : [];;
     const febBirds = city === "Austin" ? birdsByCity.austin.feb :
         city === "Dallas" ? birdsByCity.dallas.feb :
             city === "Houston" ? birdsByCity.houston.feb :
                 city === "San Antonio" ? birdsByCity.sanAntonio.feb : [];
-    const marBirds = [birds.paintedBunting, birds.rubyThroatedHummingbird];
-    const aprBirds = [birds.paintedBunting, birds.rubyThroatedHummingbird];
-    const mayBirds = [birds.paintedBunting, birds.rubyThroatedHummingbird];
+    const marBirds = city === "Austin" ? birdsByCity.austin.mar :
+        city === "Dallas" ? birdsByCity.dallas.mar :
+            city === "Houston" ? birdsByCity.houston.mar :
+                city === "San Antonio" ? birdsByCity.sanAntonio.mar : [];
+    const aprBirds = city === "Austin" ? birdsByCity.austin.apr :
+        city === "Dallas" ? birdsByCity.dallas.apr :
+            city === "Houston" ? birdsByCity.houston.apr :
+                city === "San Antonio" ? birdsByCity.sanAntonio.apr : [];
+    const mayBirds = city === "Austin" ? birdsByCity.austin.may :
+        city === "Dallas" ? birdsByCity.dallas.may :
+            city === "Houston" ? birdsByCity.houston.may :
+                city === "San Antonio" ? birdsByCity.sanAntonio.may : [];
     const junBirds = city === "Austin" ? birdsByCity.austin.jun :
         city === "Dallas" ? birdsByCity.dallas.jun :
             city === "Houston" ? birdsByCity.houston.jun :
                 city === "San Antonio" ? birdsByCity.sanAntonio.jun : [];
-    const julBirds = [birds.paintedBunting, birds.rubyThroatedHummingbird];
-    const augBirds = [birds.paintedBunting, birds.rubyThroatedHummingbird];
-    const sepBirds = [birds.paintedBunting, birds.rubyThroatedHummingbird];
-    const octBirds = [birds.paintedBunting, birds.rubyThroatedHummingbird];
-    const novBirds = [birds.paintedBunting, birds.rubyThroatedHummingbird];
-    const decBirds = [birds.paintedBunting, birds.rubyThroatedHummingbird];
+    const julBirds = city === "Austin" ? birdsByCity.austin.jul :
+        city === "Dallas" ? birdsByCity.dallas.jul :
+            city === "Houston" ? birdsByCity.houston.jul :
+                city === "San Antonio" ? birdsByCity.sanAntonio.jul : [];
+    const augBirds = city === "Austin" ? birdsByCity.austin.aug :
+        city === "Dallas" ? birdsByCity.dallas.aug :
+            city === "Houston" ? birdsByCity.houston.aug :
+                city === "San Antonio" ? birdsByCity.sanAntonio.aug : [];
+    const sepBirds = city === "Austin" ? birdsByCity.austin.sep :
+        city === "Dallas" ? birdsByCity.dallas.sep :
+            city === "Houston" ? birdsByCity.houston.sep :
+                city === "San Antonio" ? birdsByCity.sanAntonio.sep : [];
+    const octBirds = city === "Austin" ? birdsByCity.austin.oct :
+        city === "Dallas" ? birdsByCity.dallas.oct :
+            city === "Houston" ? birdsByCity.houston.oct :
+                city === "San Antonio" ? birdsByCity.sanAntonio.oct : [];
+    const novBirds = city === "Austin" ? birdsByCity.austin.nov :
+        city === "Dallas" ? birdsByCity.dallas.nov :
+            city === "Houston" ? birdsByCity.houston.nov :
+                city === "San Antonio" ? birdsByCity.sanAntonio.nov : [];
+    const decBirds = city === "Austin" ? birdsByCity.austin.dec :
+        city === "Dallas" ? birdsByCity.dallas.dec :
+            city === "Houston" ? birdsByCity.houston.dec :
+                city === "San Antonio" ? birdsByCity.sanAntonio.dec : [];
 
     const currentBirds = res.month === "Januray" ? janBirds :
         res.month === "February" ? febBirds :
